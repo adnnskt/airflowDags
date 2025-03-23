9f4db686181155986393ffe49502c7e52a6466e0
adnnskt <adnnskt@gmail.com>
2025-03-23 07:27:52 -0300

    DAG Trigger exemplo

---
+++ DAG_trigger.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+dag = DAG('terceira_dag', description='DAG exemplo com precedência', 
+            schedule_interval=None, start_date=datetime(2025, 3, 5), catchup=False)
+
+task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag)
+task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag)
+task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag)
+
+
+[task1, task2] >> task3 #Formato em precedência
