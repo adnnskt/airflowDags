5dc9a634352838b337fd3faee8f436cec8622c29
adnnskt <adnnskt@gmail.com>
2025-03-23 07:34:48 -0300

    aplicando trigger_rule

--- DAG_trigger.py
+++ DAG_trigger.py
@@ -2,12 +2,13 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
-dag = DAG('terceira_dag', description='DAG exemplo com precedência', 
+dag = DAG('trigger_dag', description='DAG exemplo de utilização de trigger em nivel de task', 
             schedule_interval=None, start_date=datetime(2025, 3, 5), catchup=False)
 
 task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag)
 task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag)
-task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag)
+task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag
+                     , trigger_rule='one_failed')
 
 
 [task1, task2] >> task3 #Formato em precedência
