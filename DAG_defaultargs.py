63f3717a4f8b2b863ce967c86fa9bd95fcf35d19
adnnskt <adnnskt@gmail.com>
2025-04-02 18:14:12 -0300

    update dags

--- DAG_defaultargs.py
+++ DAG_defaultargs.py
@@ -1,13 +1,26 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from datetime import datetime
-from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'depends_on_past': False,
+    'start_date': datetime(2025, 3, 5),
+    'email': ['teste@teste.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=10),
+}
+
 
 dag = DAG('dag_defaultargs', description='DAG exemplo de dicionario como defaultargs.', 
-            schedule_interval=None, start_date=datetime(2025, 3, 5), catchup=False)
+            default_args=default_args,
+            schedule_interval=None, start_date=datetime(2025, 3, 5), catchup=False, 
+            default_view='graph')
 
 task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag)
-task2 = TriggerDagRunOperator(task_id="tsk2", trigger_dag_id="dag_called", dag=dag)
+task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag)
+task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag)
 
 
-task1 >> task2
+task1 >> task2 >> task3
