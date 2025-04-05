5a308dd907ddf2046839d1bcc130aa4c0d5a1fbd
adnnskt <adnnskt@gmail.com>
2025-04-05 08:04:01 -0300

    Crianfo DAG xcom de exemplo

---
+++ DAG_xcom.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+
+dag = DAG('dag_run_dag', description='DAG exemplo que executa uma outra dag.', 
+            schedule_interval=None, start_date=datetime(2025, 3, 5), catchup=False)
+
+task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag)
+task2 = TriggerDagRunOperator(task_id="tsk2", trigger_dag_id="dag_called", dag=dag)
+
+
+task1 >> task2
