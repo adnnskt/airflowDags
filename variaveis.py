9d7372dd0b33dc4dbb5fffff2ab1320633dd7770
adnnskt <adnnskt@gmail.com>
2025-04-15 18:31:14 -0300

    add variaves file

--- variaveis.py
+++ variaveis.py
@@ -1,25 +1,18 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
 
 
-dag = DAG('dag_xcom', description='DAG exemplo de utilização do xcom.', 
+dag = DAG('dag_variaveis', description='DAG exemplo de utilização de variaveis.', 
             schedule_interval=None, start_date=datetime(2025, 3, 5), catchup=False)
 
 
-def task_write(**kwargs):
-    ti = kwargs['ti']
-    ti.xcom_push(key='valorxcom1', value=10200)
-    
+def print_variable(**kwargs):
+    minha_var = Variable.get("minhavar")
+    print(f'Valor da variavel: {minha_var}')
 
-task1 = PythonOperator(task_id="tsk1", python_callable=task_write  , dag=dag)
+task1 = PythonOperator(task_id="tsk1", python_callable=print_variable  , dag=dag)
 
-def task_read(**kwargs):
-    ti = kwargs['ti']
-    valor = ti.xcom_pull(key='valorxcom1')
-    print(f'Valor do xcom: {valor}')
-
-
-task2 = PythonOperator(task_id="tsk2", python_callable=task_read, dag=dag)
-
-task1 >> task2
+task1
