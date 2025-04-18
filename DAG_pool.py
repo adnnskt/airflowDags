27ee16a7f49487e42a416b820ba80571878f9398
adnnskt <adnnskt@gmail.com>
2025-04-18 09:24:05 -0300

    add content in DAG

---
+++ DAG_pool.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+
+dag = DAG('dag_pool', description='DAG exemplo de utilização de pools.', 
+            schedule_interval=None, start_date=datetime(2025, 3, 5), catchup=False)
+
+task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag, pool="mypool")
+task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag, pool="mypool")
+task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag, pool="mypool")
+task4 = BashOperator(task_id="tsk4", bash_command="sleep 5", dag=dag, pool="mypool")
+
