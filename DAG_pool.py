1a0a36825b31a24aa31b0f2bad1a1fec78e60653
adnnskt <adnnskt@gmail.com>
2025-04-18 09:33:21 -0300

    add prioryty to pool

--- DAG_pool.py
+++ DAG_pool.py
@@ -7,7 +7,7 @@ dag = DAG('dag_pool', description='DAG exemplo de utilização de pools.',
             schedule_interval=None, start_date=datetime(2025, 3, 5), catchup=False)
 
 task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag, pool="mypool")
-task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag, pool="mypool")
+task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag, pool="mypool", prioryty_weight=5)
 task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag, pool="mypool")
-task4 = BashOperator(task_id="tsk4", bash_command="sleep 5", dag=dag, pool="mypool")
+task4 = BashOperator(task_id="tsk4", bash_command="sleep 5", dag=dag, pool="mypool", prioryty_weight=10)
 
