89581b48183ac161b5376916a51d58e9e8080094
adnnskt <adnnskt@gmail.com>
2025-04-28 19:25:53 -0300

    fix dag issue

--- DAG_branch.py
+++ DAG_branch.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-import airflow.operators.python_operator as PythonOperator
-import airflow.operators.branch_operator as BranchOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import BranchPythonOperator
 import random
 from datetime import datetime
 
@@ -13,8 +13,27 @@ dag = DAG('dag_branch', description='DAG exemplo de utilização de branch.',
 def gera_numero_aleatorio():
     return random.randint(1, 100)
 
-task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag, pool="mypool")
-task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag, pool="mypool", priority_weight=5)
-task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag, pool="mypool")
-task4 = BashOperator(task_id="tsk4", bash_command="sleep 5", dag=dag, pool="mypool", priority_weight=10)
+gera_numero_aleatorio_task = PythonOperator(
+    task_id='gera_numero_aleatorio_task',
+    python_callable = gera_numero_aleatorio, dag=dag)
 
+def avalia_numero_aleatorio(**context):
+    number = context['task_instance'].xcom_pull(task_ids='gera_numero_aleatorio_task')
+    if number % 2 == 0:
+        return 'task1'
+    else:
+        return 'task2'
+
+branch_task = BranchPythonOperator(
+    task_id="branch_task",
+    python_callable=avalia_numero_aleatorio,
+    provide_context=True,
+    dag=dag
+)
+
+task1 = BashOperator(task_id="tsk1", bash_command='echo "Número Par"', dag=dag)
+task2 = BashOperator(task_id="tsk2", bash_command='echo "Número Impar"', dag=dag)
+
+gera_numero_aleatorio_task >> branch_task
+branch_task >> task1
+branch_task >> task2
